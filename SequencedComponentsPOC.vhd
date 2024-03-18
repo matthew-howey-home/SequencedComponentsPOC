e536a7d85463aaaa55b4f00e090ce4fd6a8042d8
Matthew Howey <matthew.howey@yahoo.co.uk>
2024-03-18 11:46:12 +0000

    add instance of internal oscillator to project

--- SequencedComponentsPOC.vhd
+++ SequencedComponentsPOC.vhd
@@ -9,6 +9,10 @@ entity SequencedComponentsPOC is
 end SequencedComponentsPOC;
 
 architecture Behavioral of SequencedComponentsPOC is
+
+signal connected_to_oscena	: std_logic;
+signal fast_clock				: std_logic;
+
 component Internal_Oscillator is
 	port (
 		clkout : out std_logic;
@@ -18,4 +22,14 @@ end component Internal_Oscillator;
 
 begin
 	POWER <= '1';
+	
+	-- we always want to be connected to oscena (the internal oscillator)
+	connected_to_oscena <= '1';
+	
+	Internal_Oscillator_Instance : Internal_Oscillator
+		port map (
+			clkout => fast_clock,
+			oscena => connected_to_oscena
+      );
+
 end Behavioral;
