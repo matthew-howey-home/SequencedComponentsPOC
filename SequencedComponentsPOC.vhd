146d48a228797d1be7fff12829206d8451aea8e2
Matthew Howey <matthew.howey@yahoo.co.uk>
2024-03-18 10:17:15 +0000

    consistent tabs in main file

--- SequencedComponentsPOC.vhd
+++ SequencedComponentsPOC.vhd
@@ -3,12 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity SequencedComponentsPOC is
-    Port (
-		  POWER : out STD_LOGIC
-    );
+	Port (
+		POWER : out STD_LOGIC
+	);
 end SequencedComponentsPOC;
 
 architecture Behavioral of SequencedComponentsPOC is
 begin
-	 POWER <= '1';
+	POWER <= '1';
 end Behavioral;
