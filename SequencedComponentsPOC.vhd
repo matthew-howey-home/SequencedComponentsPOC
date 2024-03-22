d1d100cd39a36285b598839f8947603cb3ba3df2
Matthew Howey <matthew.howey@yahoo.co.uk>
2024-03-22 20:22:08 +0000

    correct POC file by copying code from previous working project

--- SequencedComponentsPOC.vhd
+++ SequencedComponentsPOC.vhd
@@ -3,116 +3,102 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity SequencedComponentsPOC is
-	Port (
-		DATA_INPUT		: in std_logic;
+    Port (
+		  RESET				: in STD_LOGIC;
+		
+		  INTERNAL_LED_0 : out STD_LOGIC;
+        INTERNAL_LED_1 : out STD_LOGIC;
+		  INTERNAL_LED_2 : out STD_LOGIC;
+		  INTERNAL_LED_3 : out STD_LOGIC;
+		  INTERNAL_LED_4 : out STD_LOGIC;
+		  INTERNAL_LED_5 : out STD_LOGIC;
+		  INTERNAL_LED_6 : out STD_LOGIC;
+		  INTERNAL_LED_7 : out STD_LOGIC;
 
-		POWER				: out std_logic;
-		INTERNAL_LED_0	: out std_logic;
-		INTERNAL_LED_1	: out std_logic;
-		INTERNAL_LED_2	: out std_logic;
-		INTERNAL_LED_3 : out std_logic;
-		INTERNAL_LED_4 : out std_logic;
-		INTERNAL_LED_5 : out std_logic;
-		INTERNAL_LED_6 : out std_logic;
-		INTERNAL_LED_7 : out std_logic
-	);
+		  POWER : out STD_LOGIC
+		  
+		  -- DATA_INPUT : in STD_LOGIC
+    );
 end SequencedComponentsPOC;
 
 architecture Behavioral of SequencedComponentsPOC is
 
-signal connected_to_oscena	: std_logic;
-signal fast_clock				: std_logic;
-signal slow_clock				: std_logic;
-signal input_enable			: std_logic;
-signal register_output		: std_logic_vector(7 downto 0);
-signal register_input		: std_logic_vector(7 downto 0);
-signal add_carry_out			: std_logic;
+	component Internal_Oscillator is
+		port (
+			clkout : out std_logic;        -- clk
+			oscena : in  std_logic			 -- oscena
+		);
+	end component Internal_Oscillator;
 
-component Internal_Oscillator is
-	port (
-		clkout : out std_logic;
-		oscena : in  std_logic
-	);
-end component Internal_Oscillator;
-
-component SlowClockGenerator is
-	port (
-		FastClock   : in std_logic;
-		SlowClock   : out std_logic;
-		InputEnable : out std_logic
-	);
-end component SlowClockGenerator;
-
-component Eight_Bit_Register is
-	port (
-		Data_Input		: in std_logic_vector(7 downto 0);
-		Input_Enable	: in std_logic;
-		Clock				: in std_logic;
-		Output_Enable	: in std_logic;
-
-      Output			: out std_logic_vector(7 downto 0)
-	);
-end component Eight_Bit_Register;
-
-component ADD_Component is
-	port (
-		input_1		: in std_logic_vector(7 downto 0);
-		input_2		: in std_logic_vector(7 downto 0);
-		carry_in		: in std_logic;
-		output		: out std_logic_vector(7 downto 0);
-      carry_out	: out std_logic
-	);
-end component ADD_Component;
-
-
+	signal CONNECTED_TO_clkout : std_logic;
+   signal CONNECTED_TO_oscena : std_logic;
+	signal CLOCK_INPUT			: std_logic;
+	signal INPUT_ENABLE			: std_logic;
+	
+	signal register_output		: std_logic_vector(7 downto 0);
+	signal register_input	   : std_logic_vector(7 downto 0);
+	signal add_output				: std_logic_vector(7 downto 0);
+	signal add_carry_out			: std_logic;
+	-- signal NOT_RESET : std_logic;
+	-- signal RESET: std_logic;
+	
 begin
-	POWER <= '1';
+	CONNECTED_TO_oscena <= '1';
 	
-	-- we always want to be connected to oscena (the internal oscillator)
-	connected_to_oscena <= '1';
+	 Internal_Oscillator_Instance : Internal_Oscillator
+        port map (
+            clkout => CONNECTED_TO_clkout,
+            oscena => CONNECTED_TO_oscena
+        );
 	
-	Internal_Oscillator_Instance : Internal_Oscillator
-		port map (
-			clkout => fast_clock,
-			oscena => connected_to_oscena
-		);
-
-	SlowClockGeneratorInstance : SlowClockGenerator
-		port map (
-			FastClock	=>	fast_clock,
-			InputEnable	=>	input_enable,
-			SlowClock	=>	slow_clock
-		);
-
-	Eight_Bit_Register_Instance : Eight_Bit_Register
-		port map (
-			Data_Input		=> register_input,
-			Input_Enable	=> input_enable,
-			Clock				=> slow_clock,
-			Output_Enable	=> '1',
-
-			Output			=> register_output
-		);
+	SlowClockGenerator: entity work.SlowClockGenerator
+	port map (
+		  FastClock	=> CONNECTED_TO_clkout,
+		  SlowClock => CLOCK_INPUT,
+		  InputEnable => INPUT_ENABLE
+   );
+	
+	Eight_Bit_Register: entity work.Eight_Bit_Register
+	port map (
+		  Data_Input	 => register_input,
+        Input_Enable	 => INPUT_ENABLE,
+		  Clock		    => CLOCK_INPUT,
+		  Output_Enable => '1',
+        Output		    => register_output 
+   );
+	
+	ADD_Component: entity work.ADD_Component
+	port map (
+		  input_1	=> register_output,
+        input_2	=> "00000001",
+		  carry_in	=> '0',
+        Output		=> register_input,
+		  -- Output 	=> register_input,
+		  carry_out  => add_carry_out
+   );
+	
+	--Two_To_One_Byte_Mux: entity work.Two_To_One_Byte_Mux
+	-- port map (
+	--   input_1	 		=> add_output,
+	-- 	input_2		 	=> "00000000",
+      
+   --   selector	 		=> RESET,
 		
-	ADD_Component_Instance : ADD_Component
-		port map (
-			input_1		=> register_output,
-			input_2		=> "00000001",
-			carry_in		=> '0',
-			
-			output		=> register_input,
-			carry_out	=> add_carry_out
-		);
-			
-
-	-- all outputs inverted as 0 is lit and 1 is unlit
-	INTERNAL_LED_0 <= not register_output(0);
-	INTERNAL_LED_1 <= not register_output(1);
-	INTERNAL_LED_2 <= not register_output(2);
-	INTERNAL_LED_3 <= not register_output(3);
-	INTERNAL_LED_4 <= not register_output(4);
-	INTERNAL_LED_5 <= not register_output(5);
-	INTERNAL_LED_6 <= not register_output(6);
-	INTERNAL_LED_7 <= not register_output(7);
+	--	output			=> register_input
+   -- );
 	
+
+	 POWER <= '1';
+	 
+	 -- all outputs are inverted as for some stupid reason someone thought it was a good idea to have the LED lit for 0 and off for 1.
+	 INTERNAL_LED_0 <= not register_output(0);
+	 INTERNAL_LED_1 <= not register_output(1);
+	 INTERNAL_LED_2 <= not register_output(2);
+	 INTERNAL_LED_3 <= not register_output(3);
+	 INTERNAL_LED_4 <= not register_output(4);
+	 INTERNAL_LED_5 <= not register_output(5);
+	 INTERNAL_LED_6 <= not register_output(6);
+	 -- INTERNAL_LED_7 <= not register_output(7);
+	 INTERNAL_LED_7 <= not RESET; 
+
 end Behavioral;
